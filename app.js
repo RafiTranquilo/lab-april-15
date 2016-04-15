1ba0dc254c51e2047807f01d8b1488184dd18bd3
Rafi Ronquillo <rafironquillo@gmail.com>
2016-04-15 15:11:31 -0700

    finished problem 3 with use of an array.

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 function sum(one, two){
-  var equalsTo = (one + two);
-  console.log('"The sum of ' + one + ' and ' + two + ' is ' + equalsTo + '."');
+  return(one + two);
+  // console.log('"The sum of ' + one + ' and ' + two + ' is ' + equalsTo + '."');
 }
 // "The sum of 4 and 7 is 11."
 
 function multiply(one, two){
-  var times = (one * two);
-  console.log('"The product of ' + one + ' and ' + two + ' is ' + times + '."');
+  return(one * two);
+  // console.log('"The product of ' + one + ' and ' + two + ' is ' + times + '."');
 }
 
 // "The product of 4 and 7 is 28."
+
+function sumAndMultiply(one,two,three){
+  var output = [];
+  output.push(sum(sum(one,two),three));
+  output.push(multiply(multiply(one,two),three));
+  console.log('"' + one + 'and' + two + ' and ' + three + ' sum to ' + output[0] + '"');
+  console.log('"' + ' the numbers ' + one + ' and ' + two + ' and ' + three + ' have a product of ' + output[1] + '"');
+  return output;}
