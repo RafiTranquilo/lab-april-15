514e6310c88f5d7c65b460b68193220c79eafb5a
Rafi Ronquillo <rafironquillo@gmail.com>
2016-04-15 14:07:53 -0700

    added a second function called multiply.

--- app.js
+++ app.js
@@ -3,3 +3,10 @@ function sum(one, two){
   console.log('"The sum of ' + one + ' and ' + two + ' is ' + equalsTo + '."');
 }
 // "The sum of 4 and 7 is 11."
+
+function multiply(one, two){
+  var times = (one * two);
+  console.log('"The product of ' + one + ' and ' + two + ' is ' + times + '."');
+}
+
+// "The product of 4 and 7 is 28."
